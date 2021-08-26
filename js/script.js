748eb674c69969a3e2a7cfb48da95c704456bb04
fernandezpaulo0724 <fernandezpaulo0724@gmail.com>
2021-08-26 23:27:23 +0800

    added activity code

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let tuitt = ['Charles', 'Paul', 'Sef', 'Alex', 'Paul'];
+
+
+
+function addName(name, arr){
+	if (arr.includes(name)){
+        console.log("Name already exists.")
+    } else {
+        arr.push(name)
+        console.log("Name has been added to the list.")
+    }
+}
+ 
+
+
